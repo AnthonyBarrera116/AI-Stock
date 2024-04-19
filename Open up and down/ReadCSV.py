21543b6ee7ddf750188ae959fb46f2a7211a0ec3
Anthony Barrera <55001338+AnthonyBarrera116@users.noreply.github.com>
2024-04-18 22:40:15 -0400

    Delete Open up and down directory

--- Open up and down/ReadCSV.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-#_______________________________________Read CSV Data___________________________________________________
-def read(stock,cd):
-
-    # Reading stock CSV
-    df = pd.read_csv(cd + "\\CSV\\" + stock + ".csv")
-
-    # returns dataframe
-    return df
